2104c4afa030dd92316cf7f92857db5509b98970
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-05-30 16:17:34 -0300

    projeto iniciado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NovaNoticiaComponent } from './components/pages/nova-noticia/nova-noticia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'noticia/nova', component: NovaNoticiaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pages/nova-noticia/nova-noticia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-noticia',
+  templateUrl: './nova-noticia.component.html',
+  styleUrls: ['./nova-noticia.component.css']
+})
+export class NovaNoticiaComponent {
+
+}
